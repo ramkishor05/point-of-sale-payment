f55114fea26984b60619daab851a255aa582ab47
Ram Kishor <ram@omniesolutions.com>
2024-02-09 19:06:47 +0530

    ok

---
+++ src/main/java/com/brijframework/payment/config/SwaggerViewConfig.java
@@ -0,0 +1,46 @@
+package com.brijframework.payment.config;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerViewConfig {
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.withClassAnnotation(RestController.class)).paths(PathSelectors.any())
+				.build().securitySchemes(Arrays.asList(apiKey())).useDefaultResponseMessages(false)
+				.securityContexts(Collections.singletonList(securityContext()));
+
+	}
+
+	private ApiKey apiKey() {
+		return new ApiKey("APIKey", "Authorization", "header");
+	}
+
+	private springfox.documentation.spi.service.contexts.SecurityContext securityContext() {
+		return springfox.documentation.spi.service.contexts.SecurityContext.builder().securityReferences(defaultAuth()).forPaths(PathSelectors.regex("/.*")).build();
+	}
+
+	private List<SecurityReference> defaultAuth() {
+		final AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+		final AuthorizationScope[] authorizationScopes = new AuthorizationScope[] { authorizationScope };
+		return Collections.singletonList(new SecurityReference("APIKey", authorizationScopes));
+	}
+}
